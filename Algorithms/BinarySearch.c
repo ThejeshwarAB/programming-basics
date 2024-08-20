d002d0c25253e5c8311489deb93d4b4394659131
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2024-08-20 16:58:15 +0530

    Update BinarySearch.c

--- Algorithms/BinarySearch.c
+++ Algorithms/BinarySearch.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-int binarySearch(int arr[],int n,int k){
-    int ll=0,ul=n-1;
-    int mid=(ll+ul)/2;
-	while(ll<ul){
-	    if(arr[mid]==k)
-	    return mid+1;
-	    else if(arr[mid]<k)
-	    ll+=1;
-	    else
-	    ul-=1;
-	}
-	return -1;
+int binarySearch(int arr[], int n, int k) {
+    int ll = 0, ul = n - 1;
+    while (ll <= ul) {
+        int mid = (ll + ul) / 2;
+        if (arr[mid] == k)
+            return mid + 1;  // Return position (1-based index)
+        else if (arr[mid] < k)
+            ll = mid + 1;
+        else
+            ul = mid - 1;
+    }
+    return -1;  // Element not found
 }
 
 int main() {
-    int arr[10],n,k,i;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-		scanf("%d",&arr[i]);
-	scanf("%d",&k);
-	printf("%d",binarySearch(arr,n,k));
+    int arr[10], n, k, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    scanf("%d", &k);
+    printf("%d", binarySearch(arr, n, k));
     return 0;
 }
